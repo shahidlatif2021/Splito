f3b3a70be6c4348697521b3c59ee3e83a93a4e54
Shahid Latif <shahid.latif@seera.sa>
2022-04-13 15:47:30 +0400

    add sample test case

---
+++ SplitoUITests/SplitoUITests.swift
@@ -0,0 +1,16 @@
+//
+//  SplitoUITests.swift
+//  SplitoUITests
+//
+//  Created by Shahid Latif on 13/04/2022.
+//
+
+import XCTest
+
+class SplitoUITests: XCTestCase {
+
+    func testApp() {
+        // Write your test cases
+    }
+
+}
